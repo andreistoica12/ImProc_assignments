c1b9ee9aa3eb86f1dccb5eee92da1c2879c95d1f
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-02-25 20:18:35 +0100

    ex 2c

--- lab1/ex2/ex2.m
+++ lab1/ex2/ex2.m
@@ -3,10 +3,12 @@ clear all; % remove items from workspace
 
 a = 1;
 b = 1;
+c = 0;
 
 imname='images\peppers'; % name of the input image
-minOut = 0;
-maxOut = 255;
+L = 256;
+minOut = 102; %0;
+maxOut = 153;%L-1;
 
 inputfile = [imname,'.pgm'];
 imgIn = imread(inputfile); % read input image
@@ -25,8 +27,24 @@ end
 % Exercise 2b
 if b==1
     equalized = histogramEqualization(imgIn, minOut, maxOut);
+    min(equalized(:))
+    max(equalized(:))
     figure()
     imshow(equalized);
     title(['Equalized: [', num2str(min(equalized(:))), ', ', num2str(max(equalized(:))), ']'])
 end
 % Exercise 2c
+if a==1 && b==1 && c==1
+     figure()
+     origHist = histcounts(imgIn(:), [0:L]);
+     bar(origHist);
+     title('Original');
+     figure()
+     stretchedHist =  histcounts(stretched(:), [0:L]);
+     bar(stretchedHist);
+     title('Contrast Stretching');
+     figure()
+     equalizedHist =  histcounts(equalized(:), [0:L]);
+     bar(equalizedHist);
+     title('Histogram Equalization');
+end
--- lab1/ex2/histogramEqualization.m
+++ lab1/ex2/histogramEqualization.m
@@ -3,7 +3,7 @@ function imgOut = histogramEqualization(imgIn, minOut, maxOut)
     [height, width] = size(imgIn);
     N = height * width;
     h = histcounts(imgIn(:), [0:L]);    % freq of values in possible range
-    cdf = cumsum(h)/N;                  % normalized cumulative frequency 
+    cdf = cumsum(h)/N;                  % normalized cumulative frequency
     P = (L-1)*cdf;
     % normalize for the given range
     minIn = min(P);
