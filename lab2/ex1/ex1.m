e1d9c4f1ce6b27a02b3953d0fd4d0118d6eb261f
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-12 15:28:11 +0100

    ex 1b

--- lab2/ex1/ex1.m
+++ lab2/ex1/ex1.m
@@ -1,12 +1,23 @@
 clear all;
 
-a=1;
-b=0;
+a=0;
+b=1;
 c=0;
+
+img = imread('images/lena.pgm');
+
 
 % Exercise 1a
 if a==1
-img = imread('images/lena.pgm');
 img = thresholdImage(100, img, 0, 255);
 imshow(img);
 end
+
+% Exercise 1b
+if b==1
+    epsilon = 0.1;
+    th = iterativeThreshold(img, epsilon);
+    img = thresholdImage(th, img, 0, 255);
+    figure();imshow(img);
+end
+
---
+++ lab2/ex1/iterativeThreshold.m
@@ -0,0 +1,15 @@
+function th = iterativeThreshold(imgIn, epsilon)
+th = mean(imgIn, 'all'); % first meaningful t
+while true 
+    muF = mean(imgIn(imgIn >= th), 'all');
+    muB = mean(imgIn(imgIn < th), 'all');
+    thOld = th;
+    th = (muF+muB)/2;
+    if abs(th-thOld) < epsilon
+        break;
+    end
+end
+end
+
+
+
