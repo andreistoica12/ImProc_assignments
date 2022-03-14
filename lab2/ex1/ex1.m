d95ec591667d61a5043ac18708c32266b8fdf8e2
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-14 21:58:08 +0100

    finalize ex1d

--- lab2/ex1/ex1.m
+++ lab2/ex1/ex1.m
@@ -2,9 +2,8 @@ clear all;
 
 a=0;
 b=0;
-c=1;
-
-img = imread('images/lena.pgm');
+c=0;
+d=0;
 
 
 % Exercise 1a
@@ -22,6 +21,39 @@ end
 % Exercise 1c
 if c==1
     th = otsuThreshold(img);
+end
+% Exercise 1d
+if d==1
+    
+    names = ["vasalis", "dice", "lena", "cameraman", "peppers", "grain", "bacteria" ];
+    ths = [32,64,128,192];
+    tuples = zeros(size(names,2), size(ths,2));
+     for i=1:size(names,2)
+         img = imread(strcat("images/", names(i),".pgm"));
+         for t=1:size(ths,2)
+         out = thresholdImage(ths(t), img, 0, 255);
+         foreground = size(find(out(:) == 255),1);
+         %imwrite(out, strcat("out/", names(i), "_", num2str(ths(t)), "_", num2str(foreground), ".png"));
+         tuples(i, t) = foreground; 
+         end
+     end
+     epsilon = 0.1;
+     tuples2 = zeros(size(names,2), 4);
+    for i=1:size(names,2)
+        img = imread(strcat("images/", names(i),".pgm"));
+        th = iterativeThreshold(img, epsilon);
+        out = thresholdImage(th, img, 0, 255);
+        foreground = size(find(out(:) == 255),1);
+        tuples2(i, 1) = th;
+        tuples2(i, 2) = foreground;
+        %imwrite(out, strcat("out/", names(i), "_", "iter_", num2str(th), "_", num2str(foreground), ".png"));
+        th = otsuThreshold(img);
+        out = thresholdImage(th, img, 0, 255);
+        foreground = size(find(out(:) == 255),1);
+        tuples2(i, 3) = th;
+        tuples2(i, 4) = foreground;
+        %imwrite(out, strcat("out/", names(i), "_", "otsu_", num2str(th), "_", num2str(foreground), ".png"));
+    end
 end
 
 
