ca2dbe3219dd398c9663c8ea213b2585cf25d80d
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-14 23:45:42 +0100

    ex2 erosion

---
+++ lab2/ex2/erosionNfold.m
@@ -0,0 +1,7 @@
+function imgOut = erosionNfold(n, connectivity, imgIn)
+if connectivity == 4
+ imgOut =  bwdist(~imgIn,'cityblock') >= n;
+else
+ imgOut =  bwdist(~imgIn,'chessboard') >= n; 
+end
+end
--- lab2/ex2/ex2.m
+++ lab2/ex2/ex2.m
@@ -1,18 +1,28 @@
 clear all;
 in =  imread("images/bacteria.pgm");
-in = im2bw(in, 0.5);
-
-n = 10;
-
-%% repeated dilation
+in = im2bw(in, 0.3);
+figure();imshow(in);
+n = 3;
 se4 = strel("diamond", 1);
 se8 = strel("square", 3);
 out=in;
+
+%% repeated dilation
 for i=1:n
 out= imdilate(in, se4);
 end
-figure();imshow(out);
+%figure();imshow(out);
 
 %% dilation with distance transform
 out = dilationNfold(n, 4, in);
+%figure();imshow(out);
+
+%% repeated erosion
+for i=1:n
+out= imerode(in, se8);
+end
+figure();imshow(out);
+
+%% erosion with distance transform 
+out = erosionNfold(n,8,in);
 figure();imshow(out);
