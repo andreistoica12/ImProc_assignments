19a0e254c31f0a307aca08952bf15643503cc25f
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-27 13:09:36 +0200

    Centered fft for 1f in ex1 script

--- lab3/ex1/ex1.m
+++ lab3/ex1/ex1.m
@@ -6,11 +6,36 @@ D0=60;
 % img = randi(255, M,N);
 
   img = imread("..\..\images\characters.pgm");
+
+img = padarray(img, [10 10],224,'both');
+ %figure(); imshow(img);
   [M, N]= size(img);
+  img=double(img);
   
-  H=highPassButterworthFilter(D0, n, M,N);
+  P=2*M; Q=2*N;
+  imgPadded=zeros(P,Q);
+  imgPadded(1:M, 1:N) = img;
+  for i=0:(M-1)
+      for j=0:(N-1)
+          imgPadded(i+1, j+1)=  imgPadded(i+1, j+1) * (-1)^(i+j);
+      end
+  end
+  F = fft2(imgPadded);
+  H=highPassButterworthFilter(D0, n, P,Q);
+  G = F .* H;
+  
+  g = real(ifft2(G));
+   for i=0:(M-1)
+      for j=0:(N-1)
+          g(i+1, j+1)=  g(i+1, j+1) * (-1)^(i+j);
+      end
+   end
+  g = g(1:M, 1:N);
+  g=uint8(g);
+  imshow(g);
+ 
+%  H=highPassButterworthFilter(D0, n, M,N);
+%  
+%  out = convolutionFilter(img, H);
+%  imshow(out);
 
-
-
-out = convolutionFilter(img, H);
-figure(); imshow(out);
