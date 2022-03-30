1190f092d08b1ec1829c5fa13b526c9325e50cb5
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-30 14:09:04 +0200

    ex1d,e,f

--- lab3/ex1/convolutionFilter.m
+++ lab3/ex1/convolutionFilter.m
@@ -1,22 +1,26 @@
 function g = convolutionFilter(img, H)
-  img=double(img);
+ if size(H) ~= 2*size(img)
+     error("Error. Size of H must be 2x size of the image.");
+ end
+  img = double(img);
   H = double(H);
   [M, N]= size(img);
   P=2*M; Q=2*N;
   
-  imgPadded=zeros(P,Q);% pad the image
+  imgPadded=zeros(P,Q);                 % pad the image
   imgPadded(1:M, 1:N) = img;
- 
-  imgPadded = shift(imgPadded, M,N);% center transform
-  F = fft2(imgPadded);
+  imgPadded = shift(imgPadded, M,N);    % center the image
   
-  G = F .* H;
-  g = real(ifft2(G));
-  g = shift(g, M,N); % uncenter transform
-  g = g(1:M, 1:N);
+  F = fft2(imgPadded);                  % frequency domain representation
+  G = F .* H;                           % convolution in frequency domain
+  
+  g = real(ifft2(G));                   % return to spatial domain
+  g = shift(g, M,N);                    % uncenter the image
+  g = g(1:M, 1:N);                      % extract the original image
  
   g=uint8(g);
-  function f = shift(f, M, N)
+  
+  function f = shift(f, M, N)           % helper to (un)center the image
   for i=0:(M-1)
       for j=0:(N-1)
           f(i+1, j+1)=  f(i+1, j+1) * (-1)^(i+j);
--- lab3/ex1/ex1.m
+++ lab3/ex1/ex1.m
@@ -1,15 +1,11 @@
 clear all;
 n=2;
-
 D0=60;
-
-
-  img = imread("..\..\images\characters.pgm");
-  %figure(); imshow(img);
-  [M, N]= size(img);
-  P=2*M; Q=2*N;
-  H=highPassButterworthFilter(D0, n, P,Q);
-  out = convolutionFilter(img, H);
-  figure(); imshow(out);
+img = imread("..\..\images\characters.pgm");
+[M, N]= size(img);
+P=2*M; Q=2*N;
+H=highPassButterworthFilter(D0, n, P,Q);
+out = convolutionFilter(img, H);
+figure(); imshow(out);
  
  
