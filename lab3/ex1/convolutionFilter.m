537565d4252a1324318ef8cf20cc390630d0fdc6
m-ariya <49652030+m-ariya@users.noreply.github.com>
2022-03-30 12:12:38 +0200

    ex1f

--- lab3/ex1/convolutionFilter.m
+++ lab3/ex1/convolutionFilter.m
@@ -1,26 +1,27 @@
-function out = convolutionFilter(img, H)
-img = double(img);
-H = double(H);
-H = ifftshift(H);
-[M, N] = size(img);
-[C, D] = size(H);
-P = M+C-1;
-Q = N+D-1;
-P=M;Q=N;
-
-imgPadded = zeros(P, Q); % zero padding
-imgPadded(1:M, 1:N) = img; 
-Hp = zeros(P, Q);
-Hp(1:C, 1:D) = H; % zero padding
-
-
-F = fft2(imgPadded, P, Q);  % FFT of image
-%F  = fftshift(F);           % center transform
-G = F .* Hp;                % product of FFTs
-%G = ifftshift(G); 
-g = ifft2(G);
-g = real(g);        % real part of inverse FFT 
-out = g(1:M, 1:N);         % unpad
-out = uint8(out);
+function g = convolutionFilter(img, H)
+  img=double(img);
+  H = double(H);
+  [M, N]= size(img);
+  P=2*M; Q=2*N;
+  
+  imgPadded=zeros(P,Q);% pad the image
+  imgPadded(1:M, 1:N) = img;
+ 
+  imgPadded = shift(imgPadded, M,N);% center transform
+  F = fft2(imgPadded);
+  
+  G = F .* H;
+  g = real(ifft2(G));
+  g = shift(g, M,N); % uncenter transform
+  g = g(1:M, 1:N);
+ 
+  g=uint8(g);
+  function f = shift(f, M, N)
+  for i=0:(M-1)
+      for j=0:(N-1)
+          f(i+1, j+1)=  f(i+1, j+1) * (-1)^(i+j);
+      end
+   end
+  end
 end
 
--- lab3/ex1/ex1.m
+++ lab3/ex1/ex1.m
@@ -2,40 +2,14 @@ clear all;
 n=2;
 
 D0=60;
-% M=10;N=10;
-% img = randi(255, M,N);
+
 
   img = imread("..\..\images\characters.pgm");
-
-img = padarray(img, [10 10],224,'both');
- %figure(); imshow(img);
+  %figure(); imshow(img);
   [M, N]= size(img);
-  img=double(img);
-  
   P=2*M; Q=2*N;
-  imgPadded=zeros(P,Q);
-  imgPadded(1:M, 1:N) = img;
-  for i=0:(M-1)
-      for j=0:(N-1)
-          imgPadded(i+1, j+1)=  imgPadded(i+1, j+1) * (-1)^(i+j);
-      end
-  end
-  F = fft2(imgPadded);
   H=highPassButterworthFilter(D0, n, P,Q);
-  G = F .* H;
-  
-  g = real(ifft2(G));
-   for i=0:(M-1)
-      for j=0:(N-1)
-          g(i+1, j+1)=  g(i+1, j+1) * (-1)^(i+j);
-      end
-   end
-  g = g(1:M, 1:N);
-  g=uint8(g);
-  imshow(g);
+  out = convolutionFilter(img, H);
+  figure(); imshow(out);
  
-%  H=highPassButterworthFilter(D0, n, M,N);
-%  
-%  out = convolutionFilter(img, H);
-%  imshow(out);
-
+ 
